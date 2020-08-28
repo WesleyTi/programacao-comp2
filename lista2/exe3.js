2d2a00bb018aa6bd8f53de9c7287867218e2ca1f
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2020-08-28 00:42:43 -0300

    Finalizando exercício 3

--- lista2/exe3.js
+++ lista2/exe3.js
@@ -1,7 +1,13 @@
 function chefe() {
     var vetor = []
     do {
-        var opcao = Number(prompt("Digite [1]. Req1 .\n [2]. Req2 .\n . [3]. Req3 .\n .[4]. Req4 . \n .[5]. Req5 . \n . [6]. Req6"))
+        var opcao = Number(prompt(`Digite:\n 
+                                 [1]. Cadastrar habitante  
+                                 [2]. Media das pessoas olhos castanhos   
+                                 [3]. Maior idade entre os habitantes  
+                                 [4]. Qtd sexo feminino etc...   
+                                 [5]. Percentual de homens  
+                                 [6]. Sair do programa`))
         switch(opcao) {
             case 1: requisito1(vetor)
                 break
@@ -13,13 +19,15 @@ function chefe() {
                 break
             case 5: requisito5(vetor)
                 break
-            case 6: console.log(`O programa será encerrado`)
+            case 6: console.log(`Encerrado o programa...`)
                 break
             default:console.log(`Opção inválida!`)
         }   
     }
     while (opcao !=6) 
+
 }
+
 function requisito1(vet) {
     var objeto = new Object()
     objeto.sexo = prompt("Informe M ou F").toUpperCase()
@@ -33,10 +41,12 @@ function requisito1(vet) {
 function requisito2(vet) {
     var soma = 0
     var qtde = 0
-    for (var i=0; vet.length; i++) {
+    for (var i=0; i < vet.length; i++) {
         if ((vet[i].olhos =="C") && (vet[i].altura > 1.60)) {
             soma = soma + vet[i].idade
             qtde++
+        } else {
+            console.log(`Nenhum habitante cadastrado com olhos castanhos `)
         }
     }
     if (qtde != 0) {
@@ -45,13 +55,35 @@ function requisito2(vet) {
 }
 
 function requisito3(vet) {
-
+    var maiorIdade = vet[1].idade
+    for (var i = 0; i < vet.length; i++) {
+        if (vet[i].idade >= maiorIdade) {
+            maiorIdade = vet[i].idade
+        }
+        console.log(`A maior entre os habitantes: ${maiorIdade}`)
+    }
 }
 
 function requisito4(vet) {
+    var qtd = 0
+    for (var i = 0; i < vet.length; i++) {
+        if (((vet[i].sexo =="F") && (vet[i].idade > 19) && (vet[i].idade < 46)) 
+            || ((vet[i].olhos =="V") && (vet[i].altura < 170))) {
 
+            qtd++
+        }
+    }
+    console.log(`A quantidade do sexo feminino entre 20 e 45 anos, ou que tenham olhos verdes e altura inferior a 1.70m, é: ${qtd}`)
 }
 
 function requisito5(vet) {
-
+    var qtdHomens = 0 
+    for (var i = 0; i < vet.length; i++) {
+        if (vet[i].sexo == "M") {
+            qtdHomens++
+        }
+    }
+    console.log(`Percentual de homens: ${qtdHomens * 100 / vet.length}`)
 }
+
+chefe()
